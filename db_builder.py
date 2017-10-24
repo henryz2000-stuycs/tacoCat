bebba36e24244485248e7ae5a59b62d13590152c
henryz2000 <hzheng3@stuy.edu>
2017-10-24 13:36:58 -0400

    create stories table code in db_builder.py

--- db_builder.py
+++ db_builder.py
@@ -6,9 +6,11 @@ db = sqlite3.connect(f, check_same_thread=False) #open if f exists, otherwise cr
 c = db.cursor()    #facilitate db ops
 
 create_users = "CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT NOT NULL);"
+create_stories = "CREATE TABLE stories (id INTEGER PRIMARY KEY, title TEXT)"
 insert_admin = "INSERT INTO users VALUES ('test', 'test')"
 
 c.execute(create_users)
+c.execute(create_stories)
 c.execute(insert_admin)
 
 #==========================================================
