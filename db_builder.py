3abf5b3e86a0df24117f415cd71a42a1a55403d5
henryz2000 <hzheng3@stuy.edu>
2017-10-24 00:22:12 -0400

    login and register complete

--- db_builder.py
+++ db_builder.py
@@ -1,40 +1,16 @@
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
 
+f="story.db"
 
-f="discobandit.db"
-
-db = sqlite3.connect(f) #open if f exists, otherwise create
+db = sqlite3.connect(f, check_same_thread=False) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
+create_users = "CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT NOT NULL);"
+insert_admin = "INSERT INTO users VALUES ('test', 'test')"
 
-#command = ""          #put SQL statement in this string
-#c.execute(command)    #run SQL statement
+c.execute(create_users)
+c.execute(insert_admin)
 
-create_courses = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);"
-create_peeps = "CREATE TABLE peeps (name TEXT, age INTEGER, id PRIMARY KEY);"
-
-courses_csv = csv.DictReader(open("courses.csv"))
-peeps_csv = csv.DictReader(open("peeps.csv"))
-
-c.execute(create_courses)
-c.execute(create_peeps)
-
-for row in courses_csv:
-    #print row
-    insert_courses = "INSERT INTO courses VALUES (\"%s\", %d, %d)" % (row['code'], int(row['mark']), int(row['id']))
-    #print insert_courses
-    c.execute(insert_courses)
-
-for row in peeps_csv:
-    #print row
-    insert_peeps = "INSERT INTO peeps VALUES (\"%s\", %d, %d)" % (row['name'], int(row['age']), int(row['id']))
-    #print insert_peeps
-    c.execute(insert_peeps)
-
-    
 #==========================================================
 
 db.commit() #save changes
--- login.py
+++ login.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, session, redirect, url_for, f
 import os
 import sqlite3
 
-USER = 'DW'
-PASS = 'n00b'
+#USER = 'DW'
+#PASS = 'n00b'
 
 SUCCESS = 1
 BAD_PASS = -1
@@ -17,35 +17,68 @@ try:
 except:
     pass
 
-def authenticate(user, passwd):
-    try:
-        pass
-    except:
-        pass
-    if user == USER:
-        if passwd == PASS:
+users = {}
+user_data = c.execute("SELECT * FROM users;")
+
+for data in user_data:
+    users[data[0]] = data[1]
+
+def authenticate(username, password):
+    if username in users.keys():
+        if password == users[username]:
             return SUCCESS
         else:
             return BAD_PASS
     else:
         return BAD_USER
 
+def check_newuser(username):
+    if username in users.keys():
+        return BAD_USER
+    return SUCCESS
+    
 @form_site.route('/')
 def root():
     if 'user' not in session:
-        return render_template('form.html', title="Login")
+        return render_template('login.html', title="Login")
     else:
         return redirect( url_for('welcome') )
 
-@form_site.route('/auth', methods=['POST'])
-def auth():
-    user = request.form['user']
-    passwd = request.form['pw']
-    
-    result = authenticate(user, passwd)
+@form_site.route('/register')
+def register():
+    if 'user' not in session:
+        return render_template('register.html', title="Register")
+    else:
+        return redirect( url_for('welcome') )
+
+@form_site.route('/createaccount', methods=['POST'])
+def create_account():
+    username = request.form['user']
+    password = request.form['pw']
+
+    result = check_newuser(username)
 
     if result == SUCCESS:
-        session['user'] = user
+        insert_user = "INSERT INTO users VALUES ('%s', '%s')" %(username, password)
+        c.execute(insert_user)
+        db.commit()
+        users[username] = password
+        flash(username + " registered.")
+        return redirect( url_for('root') )
+    elif result == BAD_USER:
+        flash("Bad Username. Try another one")
+        return redirect(url_for('register'))
+    return redirect(url_for('root'))
+    
+@form_site.route('/auth', methods=['POST'])
+def auth():
+    username = request.form['user']
+    password = request.form['pw']
+    
+    result = authenticate(username, password)
+
+    if result == SUCCESS:
+        session['user'] = username
         flash(session['user'] + " logged in.")
         return redirect( url_for('welcome') )
     if result == BAD_PASS:
@@ -72,3 +105,6 @@ def logout():
 if __name__ == '__main__':
     form_site.debug = True
     form_site.run()
+
+db.commit()
+db.close()
