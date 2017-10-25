669fe9e27fe39722f8d65249aa46ebbb997c725a
henryz2000 <hzheng3@stuy.edu>
2017-10-24 22:24:52 -0400

    working on editstory

--- db_builder.py
+++ db_builder.py
@@ -6,16 +6,35 @@ db = sqlite3.connect(f, check_same_thread=False) #open if f exists, otherwise cr
 c = db.cursor()    #facilitate db ops
 
 create_users = "CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT NOT NULL);"
-create_stories = "CREATE TABLE stories (id INTEGER PRIMARY KEY, title TEXT)"
-insert_admin = "INSERT INTO users VALUES ('test', 'test')"
+create_history = "CREATE TABLE history (username TEXT NOT NULL, id INTEGER NOT NULL, contribution TEXT);"
+create_stories = "CREATE TABLE stories (id INTEGER PRIMARY KEY, title TEXT, fullstory TEXT NOT NULL, previousupdate TEXT NOT NULL);"
 
-c.execute(create_users)
-c.execute(create_stories)
-c.execute(insert_admin)
+insert_admin = "INSERT INTO users VALUES ('test', 'test');"
+insert_admin0 = "INSERT INTO users VALUES ('test0', 'test0');"
+insert_history = "INSERT INTO history VALUES ('test', 0, 'hi');"
+insert_history0 = "INSERT INTO history VALUES ('test0', 0, 'hi0');"
+insert_stories = "INSERT INTO stories VALUES (0, 'blah', 'hi\nhi0', 'hi0');"
+
+insert_newstory = "INSERT INTO stories VALUES (1, 'new title', 'new', '')"
+
+try:
+    c.execute(create_users)
+    c.execute(create_history)
+    c.execute(create_stories)
+    c.execute(insert_admin)
+    c.execute(insert_history)
+    c.execute(insert_stories)
+
+    c.execute(insert_admin0)
+    c.execute(insert_history0)
+
+    c.execute(insert_newstory)
+except:
+    pass
 
 #==========================================================
 
 db.commit() #save changes
-db.close()  #close database
+#db.close()  #close database
 
 
--- login.py
+++ login.py
@@ -12,16 +12,40 @@ BAD_USER = -2
 form_site = Flask(__name__)
 form_site.secret_key = os.urandom(64)
 
-try:
-    execfile("db_builder.py")
-except:
-    pass
+execfile("db_builder.py")
 
-users = {}
+users = {} #{username: password}
 user_data = c.execute("SELECT * FROM users;")
 
 for data in user_data:
     users[data[0]] = data[1]
+
+#print users
+    
+history = {} #{id: {username: contribution}}
+history_data = c.execute("SELECT * FROM history;")
+
+for data in history_data:
+    try:
+        history[data[1]][data[0]] = data[2]
+    except:
+        history[data[1]] = {data[0]: data[2]}
+        
+#print history
+
+ 
+stories = {} #{id: {title: title, fullstory: fullstory, previousupdate: previousupdate}}
+stories_data = c.execute("SELECT * FROM stories;")
+
+for data in stories_data:
+    try:
+        stories[data[0]]["title"] = data[1]
+        stories[data[0]]["fullstory"] = data[2]
+        stories[data[0]]["previousupdate"] = data[3]
+    except:
+        stories[data[0]] = {"title": data[1], "fullstory": data[2], "previousupdate": data[3]}
+
+#print stories
 
 def authenticate(username, password):
     if username in users.keys():
@@ -86,6 +110,22 @@ def auth():
     elif result == BAD_USER:
         flash("Incorrect Username.")
     return redirect(url_for('root'))
+
+@form_site.route('/editstory', methods=['POST'])
+def edit_story():
+    pass
+
+@form_site.route('/choseneditstory', methods=['POST'])
+def chosen_edit_story():
+    return render_template("edit_story.html")
+
+@form_site.route('/chooseeditstory', methods=['POST'])
+def choose_edit_story():
+    new = {}
+    for story in history.keys():
+        if session['user'] not in story.keys():
+            new[story] = stories[story][title]
+    return render_template('list.html', title="Edit Stories!", new=new)
 
 @form_site.route('/welcome')
 def welcome():
