c8611abd6c5586e193ff4794e334a89cf5ba5eef
Angelica Zverovich <azverovich@stuy.edu>
2017-10-29 18:58:36 -0400

    major bug fixes and minor appearance changes/removed redundant template

--- login.py
+++ login.py
@@ -14,40 +14,43 @@ form_site.secret_key = os.urandom(64)
 
 execfile("db_builder.py")
 
-users = {} #{username: password}
-user_data = c.execute("SELECT * FROM users;")
-
-for data in user_data:
-    users[data[0]] = data[1]
+def user_dict():
+    users = {} #{username: password}
+    user_data = c.execute("SELECT * FROM users;")
+    for data in user_data:
+        users[data[0]] = data[1]
+    return users;
 
 #print users
 
-history = {} #{id: {username: contribution}}
-history_data = c.execute("SELECT * FROM history;")
+def history_dict():
+    history = {} #{id: {username: contribution}}
+    history_data = c.execute("SELECT * FROM history;")
 
-for data in history_data:
-    try:
-        history[data[1]][data[0]] = data[2]
-    except:
-        history[data[1]] = {data[0]: data[2]}
+    for data in history_data:
+        try:
+            history[data[1]][data[0]] = data[2]
+        except:
+            history[data[1]] = {data[0]: data[2]}
+    return history;
 
 #print history
 
-
-stories = {} #{id: {title: title, fullstory: fullstory, previousupdate: previousupdate}}
-stories_data = c.execute("SELECT * FROM stories;")
-
-for data in stories_data:
-    try:
-        stories[data[0]]["title"] = data[1]
-        stories[data[0]]["fullstory"] = data[2]
-        stories[data[0]]["previousupdate"] = data[3]
-    except:
-        stories[data[0]] = {"title": data[1], "fullstory": data[2], "previousupdate": data[3]}
-
+def story_dict():
+    stories = {} #{id: {title: title, fullstory: fullstory, previousupdate: previousupdate}}
+    stories_data = c.execute("SELECT * FROM stories;")
+    for data in stories_data:
+        try:
+            stories[data[0]]["title"] = data[1]
+            stories[data[0]]["fullstory"] = data[2]
+            stories[data[0]]["previousupdate"] = data[3]
+        except:
+            stories[data[0]] = {"title": data[1], "fullstory": data[2], "previousupdate": data[3]}
+    return stories;
 #print stories
 
 def authenticate(username, password):
+    users = user_dict()
     if username in users.keys():
         if password == users[username]:
             return SUCCESS
@@ -56,7 +59,9 @@ def authenticate(username, password):
     else:
         return BAD_USER
 
+
 def check_newuser(username):
+    users = user_dict()
     if username in users.keys():
         return BAD_USER
     return SUCCESS
@@ -79,12 +84,10 @@ def register():
 def create_account():
     username = request.form['user']
     password = request.form['pw']
-
     result = check_newuser(username)
-
+    users = user_dict()
     if result == SUCCESS:
-        insert_user = "INSERT INTO users VALUES ('%s', '%s')" %(username, password)
-        c.execute(insert_user)
+        c.execute("INSERT INTO users VALUES (?, ?)", (username, password))
         db.commit()
         users[username] = password
         flash(username + " registered.")
@@ -98,9 +101,7 @@ def create_account():
 def auth():
     username = request.form['user']
     password = request.form['pw']
-
     result = authenticate(username, password)
-
     if result == SUCCESS:
         session['user'] = username
         flash(session['user'] + " logged in.")
@@ -117,22 +118,28 @@ def edit_story():
     #print "TTTTTTTTT"
     #print request.form['id']
     #c.execute("DELETE FROM history WHERE contribution='hello';")
-    c.execute("INSERT INTO history VALUES ('%s', %s, '%s');" %(session['user'], request.form['id'], request.form['contribution']))
+    stories = story_dict()
+    if "user" not in session:
+        flash("You must login to edit story")
+    else:
+        print "ID= " + request.form['id']
+        c.execute("INSERT INTO history VALUES (?, ?, ?)", (session['user'], request.form['id'], request.form['contribution']))
     #c.execute("DELETE FROM
-    print "##########################################################"
-    print stories[int(request.form['id'])]["fullstory"]
-    print request.form['contribution']
-    print stories[int(request.form['id'])]["fullstory"] + request.form['contribution']
-    print "##########################################################"
-    c.execute("UPDATE stories SET fullstory='%s' WHERE id=%s;" %(stories[int(request.form['id'])]["fullstory"] + request.form['contribution'], request.form['id']))
-    c.execute("UPDATE stories SET previousupdate='%s' WHERE id=%s;" %(request.form['contribution'], request.form['id']))
-    db.commit()
-    flash("Story edited.")
+        print "##########################################################"
+        print stories[int(request.form['id'])]["fullstory"]
+        print request.form['contribution']
+        print stories[int(request.form['id'])]["fullstory"] + request.form['contribution']
+        print "##########################################################"
+        c.execute("UPDATE stories SET fullstory=? WHERE id=?", (stories[int(request.form['id'])]["fullstory"] + " " + request.form['contribution'], request.form['id']))
+        c.execute("UPDATE stories SET previousupdate=? WHERE id=?", (request.form['contribution'], request.form['id']))
+        db.commit()
+        flash("Story edited.")
     #INSERT FLASH HERE***************************************
-    return redirect( url_for('welcome') )
+        return redirect( url_for('welcome') )
 
 @form_site.route('/choseneditstory', methods=['POST'])
 def chosen_edit_story():
+    stories = story_dict()
     '''
     print request.form.keys()
     print request.form.values()
@@ -141,31 +148,41 @@ def chosen_edit_story():
     print stories
     print stories[request.form.keys()[0]]
     '''
-    previous = stories[int(request.form.values()[0])]["previousupdate"]
-    print previous
-    id = int(request.form.values()[0])
-    print "----------"
-    print id
-    print "----------"
-    return render_template("edit_story.html", id=id, title="Edit %s" %stories[int(request.form.values()[0])]["title"], previous=previous)
+    if "user" not in session:
+        flash("You must login to edit story")
+        return redirect (url_for("welcome"))
+    else:
+        previous = stories[int(request.form.values()[0])]["previousupdate"]
+        print previous
+        id = int(request.form.values()[0])
+        print "----------"
+        print id
+        print "----------"
+        return render_template("edit_story.html", id=id, title=stories[int(request.form.values()[0])]["title"], previous=previous)
 
 @form_site.route('/chooseeditstory', methods=['POST'])
 def choose_edit_story():
+    stories = story_dict()
+    history = history_dict()
     new = {}
-    #print history
-    for story in stories.keys():
-        '''
-        print "===================="
-        print story
-        print session['user']
-        print history[story].keys()
-        print "===================="
-        '''
-        if session['user'] not in history[story].keys():
+    if "user" not in session:
+        flash("You must login to edit story")
+        return redirect (url_for("welcome"))
+    else:
+        for story in stories.keys():
+            '''
+            print "===================="
+            print story
+            print session['user']
+            print history[story].keys()
+            print "===================="
+            '''
+            if session['user'] not in history[story].keys():
             #new[story] = stories[story]["title"].replace(" ", "_")
-            new[story] = stories[story]["title"]
-    print new
-    return render_template('list.html', title="Edit Stories!", new=new)
+                    new[story] = stories[story]["title"]
+        if not new:
+            flash ("There are no new stories to edit (You may only edit stories that you have not created or edited before!)")
+        return render_template('list.html', title="Edit Stories!", new=new, action="edit", actionurl = "/choseneditstory")
 
 @form_site.route('/welcome')
 def welcome():
@@ -198,9 +215,9 @@ def create():
             #print id
             #successfully updates stories database
         with db:
-            new_entry = c.execute("INSERT into stories VALUES ('%s','%s', '%s', '%s');" %(id, title, line, line))
-            update_history = c.execute("INSERT into history VALUES ('%s', '%s', '%s');" %(session['user'], id, line))
-            flash("Your story was succesfully created!")
+            c.execute("INSERT into stories VALUES (?, ?, ?, ?)", (id, title, line, line))
+            c.execute("INSERT into history VALUES (?, ?, ?)", (session['user'], id, line))
+        flash("Your story was succesfully created!")
     return redirect(url_for('welcome'))
 
 
@@ -211,15 +228,16 @@ def viewstory():
     for story in all_stories:
         story_dict[story[0]] = story[1]
     #print story_dict
-    return render_template("view_stories.html", new = story_dict)
+    return render_template("list.html", new = story_dict, action="view", actionurl = "/display")
 
 @form_site.route('/display', methods = ['POST', 'GET'])
 def displaypage():
-    idnum = request.form.get('storyid')
-    storyinfo = c.execute("SELECT * FROM stories where ID = %s;" %(idnum)).fetchall()
+    idnum = request.form.get('id')
+    print idnum
+    storyinfo = c.execute("SELECT * FROM stories where ID = ?", (idnum,)).fetchall()
     title = storyinfo[0][1]
     fullstory = storyinfo[0][2]
-    return render_template("display.html", title = title, story = fullstory) 
+    return render_template("display.html", title = title, story = fullstory)
     #return render_template("display.html", story = fullstory)
 
 if __name__ == '__main__':
